a3f82576cc0a63e2481f68db7fd725c127cdc85c
h0rn3t <vamp.null@gmail.com>
2021-04-20 10:26:42 +0300

    initial

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     # package_data={"fastapi-async-sqlalchemy": ["py.typed"]},
     zip_safe=False,
     python_requires=">=3.7",
-    install_requires=["starlette>=0.14.2", "SQLAlchemy>=1.4.9"],
+    install_requires=["starlette>=0.13.6", "SQLAlchemy>=1.4.9"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
