7aa6decca560dd0abe41557b44aec8319212c7e0
Eugene Shershen <shershen.yv@uds.ua.energy>
2024-10-17 21:28:16 +0300

    commit_on_exit fix

--- fastapi_async_sqlalchemy/middleware.py
+++ fastapi_async_sqlalchemy/middleware.py
@@ -11,11 +11,17 @@ from starlette.types import ASGIApp
 
 from fastapi_async_sqlalchemy.exceptions import MissingSessionError, SessionNotInitialisedError
 
+try:
+    from sqlalchemy.ext.asyncio import async_sessionmaker
+except ImportError:
+    from sqlalchemy.orm import sessionmaker as async_sessionmaker
+
 
 def create_middleware_and_session_proxy():
     _Session: Optional[async_sessionmaker] = None
     _session: ContextVar[Optional[AsyncSession]] = ContextVar("_session", default=None)
     _multi_sessions_ctx: ContextVar[bool] = ContextVar("_multi_sessions_context", default=False)
+    _commit_on_exit_ctx: ContextVar[bool] = ContextVar("_commit_on_exit_ctx", default=False)
 
     class SQLAlchemyMiddleware(BaseHTTPMiddleware):
         def __init__(
@@ -23,8 +29,8 @@ def create_middleware_and_session_proxy():
             app: ASGIApp,
             db_url: Optional[Union[str, URL]] = None,
             custom_engine: Optional[Engine] = None,
-            engine_args: Optional[Dict] = None,
-            session_args: Optional[Dict] = None,
+            engine_args: Dict = None,
+            session_args: Dict = None,
             commit_on_exit: bool = False,
         ):
             super().__init__(app)
@@ -54,12 +60,9 @@ def create_middleware_and_session_proxy():
             if _Session is None:
                 raise SessionNotInitialisedError
 
-            current_session = _session.get()
-            if current_session is None:
-                raise MissingSessionError
-
             multi_sessions = _multi_sessions_ctx.get()
             if multi_sessions:
+                commit_on_exit = _commit_on_exit_ctx.get()
                 task = asyncio.current_task()
                 if not hasattr(task, "_db_session"):
                     task._db_session = _Session()
@@ -73,7 +76,8 @@ def create_middleware_and_session_proxy():
                                     if future.exception():
                                         await session.rollback()
                                     else:
-                                        await session.commit()
+                                        if commit_on_exit:
+                                            await session.commit()
                                 finally:
                                     await session.close()
 
@@ -82,67 +86,38 @@ def create_middleware_and_session_proxy():
                     task.add_done_callback(cleanup)
                 return task._db_session
             else:
-                return current_session
-
-        def __call__(
-            cls,
-            session_args: Optional[Dict] = None,
-            commit_on_exit: bool = False,
-            multi_sessions: bool = False,
-        ):
-            return cls._context_manager(
-                session_args=session_args,
-                commit_on_exit=commit_on_exit,
-                multi_sessions=multi_sessions,
-            )
-
-        def _context_manager(
-            cls,
-            session_args: Dict = None,
-            commit_on_exit: bool = False,
-            multi_sessions: bool = False,
-        ):
-            return DBSessionContextManager(
-                session_args=session_args,
-                commit_on_exit=commit_on_exit,
-                multi_sessions=multi_sessions,
-            )
+                session = _session.get()
+                if session is None:
+                    raise MissingSessionError
+                return session
 
     class DBSession(metaclass=DBSessionMeta):
-        pass
-
-    class DBSessionContextManager:
         def __init__(
             self,
             session_args: Dict = None,
             commit_on_exit: bool = False,
             multi_sessions: bool = False,
         ):
+            self.token = None
+            self.multi_sessions_token = None
+            self.commit_on_exit_token = None
             self.session_args = session_args or {}
             self.commit_on_exit = commit_on_exit
             self.multi_sessions = multi_sessions
-            self.token = None
-            self.multi_sessions_token = None
-            self._session = None
 
         async def __aenter__(self):
-            if _Session is None:
+            if not isinstance(_Session, async_sessionmaker):
                 raise SessionNotInitialisedError
 
             if self.multi_sessions:
                 self.multi_sessions_token = _multi_sessions_ctx.set(True)
+                self.commit_on_exit_token = _commit_on_exit_ctx.set(self.commit_on_exit)
 
-            self._session = _Session(**self.session_args)
-            self.token = _session.set(self._session)
-            return self
-
-        @property
-        def session(self):
-            return self._session
+            self.token = _session.set(_Session(**self.session_args))
+            return type(self)
 
         async def __aexit__(self, exc_type, exc_value, traceback):
             session = _session.get()
-
             try:
                 if exc_type is not None:
                     await session.rollback()
@@ -153,6 +128,7 @@ def create_middleware_and_session_proxy():
                 _session.reset(self.token)
                 if self.multi_sessions_token is not None:
                     _multi_sessions_ctx.reset(self.multi_sessions_token)
+                    _commit_on_exit_ctx.reset(self.commit_on_exit_token)
 
     return SQLAlchemyMiddleware, DBSession
 
